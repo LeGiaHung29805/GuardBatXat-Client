02579e4d529f16772360d1d2e84ddca5a12f98b1
dihtung041100-oss <dihtung041100@gmail.com>
2026-06-26 11:44:58 +0700

    Update 2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: false,
-  allowedDevOrigins: ["localhost", "192.168.102.21", "127.0.0.1"]
+
+  allowedDevOrigins: [
+    "localhost",
+    "192.168.102.21",
+    "127.0.0.1",
+    "rounding-slate-brisket.ngrok-free.dev",
+  ],
 };
 
 export default nextConfig;
